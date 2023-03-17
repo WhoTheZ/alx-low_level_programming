4ce916f30c6022cfdf04e335a9abb79ce466e49c
WhoTheZ <fizzkhalifa1211@gmail.com>
2023-03-17 14:11:04 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,12 +5,13 @@
  *
  * Return: always 0
  */
+
 int main(void)
 {
 char i;
 
 for (i = 'a' ; i <= 'z' ; i++)
-if (i != 'q' && i != 'e') 
+if (i != 'q' && i != 'e')
 putchar(i);
 putchar('\n');
 return (0);
