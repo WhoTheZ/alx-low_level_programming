faeb55186a3daf698a62408199d751c2e8804dac
WhoTheZ <fizzkhalifa1211@gmail.com>
2023-03-17 15:19:37 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,17 +11,17 @@ int main(void)
 int i;
 int j;
 
-for (i = 0 ; i < 10 ; i++)
-for (j = i + 1 ; j < 10 ; j++)
-
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
 putchar(i + '0');
 putchar(j + '0');
-
 putchar(',');
+putchar(' ');
 
-
-
-putchar('\n');
-return (0);
+}
 }
 
+return (0);
+}
